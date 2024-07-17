245d4ef65e4020fbec2f41b8c256293faa0aaf3b
JonathanLu2005 <jonathanlu2005@yahoo.com>
2024-07-17 18:50:52 +0100

    Included header video for home page

--- Server.py
+++ Server.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/", methods=["POST","GET"])
-def homePage():
-    return render_template("homepage.html")
+def home():
+    return render_template("home.html")
+
+@app.route("/", methods=["POST","GET"])
+def aboutUs():
+    return render_template("aboutus.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
