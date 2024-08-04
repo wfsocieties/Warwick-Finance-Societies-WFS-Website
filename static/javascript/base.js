4677c092bf5fb3bdf2e3a760893806e600917db8
JonathanLu2005 <jonathanlu2005@yahoo.com>
2024-08-04 12:11:23 +0100

    Responsive background

--- static/javascript/base.js
+++ static/javascript/base.js
@@ -1,21 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Initialise AOS
+document.addEventListener('DOMContentLoaded', function () {
     AOS.init();
 
-    // Initialise VANTA
-    VANTA.WAVES({
-        el: "#mainContent",
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
-        color: 0x43d54,
-        shininess: 150.00,
-        waveHeight: 25.00,
-        waveSpeed: 1.35,
-        zoom: 0.65
-    });
+    function initVanta() {
+        if (window.vantaEffect) {
+            window.vantaEffect.destroy(); 
+        }
+
+        if (window.innerWidth > 576) {
+            window.vantaEffect = VANTA.WAVES({
+                el: "#mainContent",
+                mouseControls: true,
+                touchControls: true,
+                gyroControls: false,
+                minHeight: 200.00,
+                minWidth: 200.00,
+                scale: 1.00,
+                scaleMobile: 1.00,
+                color: 0x43d54,
+                shininess: 150.00,
+                waveHeight: 25.00,
+                waveSpeed: 1.35,
+                zoom: 0.65
+            });
+        } else {
+            document.getElementById('mainContent').style.backgroundColor = '#243282';
+        }
+    }
+
+    initVanta();
+
+    window.addEventListener('resize', initVanta);
 });
